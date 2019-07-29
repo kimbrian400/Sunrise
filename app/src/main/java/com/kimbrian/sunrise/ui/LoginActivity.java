55f473bc838a3c3e68f193208775b1c870f80d55
Brian Kimathi <kimbrian400@gmail.com>
2019-07-29 21:22:21 +0300

    Version 1.2.2.1

--- app/src/main/java/com/kimbrian/sunrise/ui/LoginActivity.java
+++ app/src/main/java/com/kimbrian/sunrise/ui/LoginActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,141 +27,103 @@ import com.kimbrian.sunrise.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText editEmail, editPassword;
-    Button btnlogin;
-    TextView textforgot;
-    TextView btnSignup;
-    NavigationView nav_view;
-
+    private EditText userMail,userPassword;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
     private FirebaseAuth mAuth;
-    ProgressBar progressBar;
-
-    private static final String TAG = "FirebaseErrors";
+    private Intent HomeActivity;
+    private ImageView loginPhoto;
+    private TextView createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        isUserLogin();
-
         setContentView(R.layout.activity_login);
 
-        //get firebase auth instance
+        createAccount = findViewById(R.id.btnSignup);
+        userMail = findViewById(R.id.editEmail);
+        userPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.btnlogin);
+        loginProgress = findViewById(R.id.progressBar);
         mAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this, com.kimbrian.sunrise.MainActivity.class);
 
-        //firebaseAuth = FirebaseAuth.getInstance();
-
-
-
-        FirebaseApp.initializeApp(this);
-        FirebaseApp.initializeApp(LoginActivity.this);
-
-        //button,textview,edittext
-        editEmail = findViewById(R.id.editEmail);
-        editPassword = findViewById(R.id.editPassword);
-        btnlogin = findViewById(R.id.btnlogin);
-        textforgot = findViewById(R.id.textforgot);
-        btnSignup = findViewById(R.id.btnSignup);
-
-
-        btnSignup.setOnClickListener(this);
-        editEmail.setOnClickListener(this);
-        editPassword.setOnClickListener(this);
-        textforgot.setOnClickListener(this);
-
-        //progressBar = findViewById(R.id.progressBarLog);
-        //progressBar.setVisibility(View.INVISIBLE);
-
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userLogin();
+                Intent haha = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(haha);
+                finish();
             }
         });
 
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (mail.isEmpty() || password.isEmpty()) {
+                    showMessage("Please Verify All Fields");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    signIn(mail,password);
+                }
+            }
+        });
 
     }
 
-    /**
-     * Checks if user is logged in
-     */
+    private void signIn(String mail, String password) {
 
+        mAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
+                if (task.isSuccessful()) {
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+                else {
+                    showMessage(task.getException().getMessage());
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
-    private void isUserLogin() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
         if (user != null) {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
+            //User is already connected  so we need to redirect him/her to home page
+            updateUI();
         }
     }
-
-
-    private void userLogin() {
-        String email = editEmail.getText().toString().trim();
-        String password = editPassword.getText().toString().trim();
-
-        //checking if email and password are empty
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Please enter password", Toast.LENGTH_LONG).show();
-            return;
-        }
-        //if the email and password are not empty display progressbar
-        //progressBar.setVisibility(View.VISIBLE);
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "Successful Log in.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(user);
-                            /*Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                                    Uri.parse("geo:0,0?q=37.423156,-122.084917 (Sunrise)"));
-                            startActivity(intent);*/
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
-    }
-
 
     @Override
     public void onClick(View view) {
-        if (view == btnlogin) {
-            finish();
 
-        }
-
-        if (view == btnSignup) {
-            finish();
-            startActivity(new Intent(this, SignupActivity.class));
-        }
-        if (view == textforgot) {
-            finish();
-            startActivity(new Intent(this, ResetPasswordActivity.class));
-        }
-         /*if (view == nav_view){
-             finish();
-             startActivity(new Intent(this, NavActivity.class));
-         }*/
     }
 }
 
